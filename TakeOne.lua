6f0bcb1bf7c5fbf31fe3b9aa7982d8f1000c62f5
Joseph Langley <mightyjo@gmail.com>
2021-05-26 00:16:46 -0400

    Add translation support
    Switch logging to LibDebugLogger
    Drop LibMarify dependency

--- TakeOne.lua
+++ TakeOne.lua
@@ -2,13 +2,12 @@ TakeOne = {
     displayName = "Take One",
     shortName = "TO",
     name = "TakeOne",
-    version = "1.0.2",
-
+    version = "1.1.0",
+    logger = nil,
+	
 }
 
 function TakeOne:CreateMenu()
-
-    self.savedVariables.debugLog = {}
 
     local panelData = {
         type = "panel",
@@ -25,7 +24,7 @@ function TakeOne:CreateMenu()
     local optionsTable = {
         {
             type = "checkbox",
-            name = "Show Debug Log",
+            name = GetString(TAKE_ONE_OPTION_DEBUG),
             getFunc = function()
                 return self.savedVariables.isDebug
             end,
@@ -39,31 +38,103 @@ function TakeOne:CreateMenu()
     LibAddonMenu2:RegisterOptionControls(self.displayName, optionsTable)
 end
 
+function TakeOne:Info(text, ...)
+    
+	if self.logger then
+	  selfLog(LibDebugLogger.LOG_LEVEL_INFO, text, ...)
+	else
+	  if ... ~= nil then
+	    text = zo_strformat(text, unpack({...}))
+	  end
+	  d( string.format("%s: %s", self.name, text) )
+	end
+	
+end
+
+function TakeOne:Debug(text, ...)
+    
+	if self.logger == nil then
+	  return
+	end
+	
+	if self.savedVariables.isDebug == false then
+	  return
+	end
+	  
+	self:Log(LibDebugLogger.LOG_LEVEL_DEBUG, text, ...)
+	
+end
+
+function TakeOne:Warn(text, ...)
+    
+	if self.logger == nil then
+	  return
+	end
+	  
+	self:Log(LibDebugLogger.LOG_LEVEL_WARNING, text, ...)
+	
+end
+
+function TakeOne:Error(text, ...)
+    
+	if self.logger == nil then
+	  return
+	end
+	  
+	self:Log(LibDebugLogger.LOG_LEVEL_ERROR, text, ...)
+	
+end
+
+function TakeOne:Log(level, text, ...)
+    if self.logger == nil then
+	  return
+	end
+	
+	local switch = {
+	  [LibDebugLogger.LOG_LEVEL_DEBUG] = function (text) self.logger:Debug(text) end,
+	  [LibDebugLogger.LOG_LEVEL_INFO] = function (text) self.logger:Info(text) end,
+	  [LibDebugLogger.LOG_LEVEL_WARNING] = function (text) self.logger:Warn(text) end,
+	  [LibDebugLogger.LOG_LEVEL_ERROR] = function (text) self.logger:Error(text) end,
+	  default = nil,
+	}
+	
+	local case = switch[level] or switch.default
+	if case then
+	  if ... ~= nil then
+	    text = zo_strformat(text, unpack({...}))
+	  end
+	  case(text)
+	end
+
+end
+
 function TakeOne:OnAddOnLoaded(event, addonName)
 
     if addonName ~= self.name then
         return
     end
     EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
-    setmetatable(TakeOne, {__index = LibMarify})
+    
+	if LibDebugLogger then
+	  self.logger = LibDebugLogger(self.name)
+	end
 
     self.savedVariables = ZO_SavedVars:NewAccountWide("TakeOneVariables", 1, nil, {})
-    self.savedCharVariables  = ZO_SavedVars:NewCharacterIdSettings("TakeOneVariables", 1, nil, {})
+	--self.savedCharVariables  = ZO_SavedVars:NewCharacterIdSettings("TakeOneVariables", 1, nil, {})
 	self:CreateMenu()
 
     LibCustomMenu:RegisterContextMenu(function(...) self:ShowContextMenu(...) end, LibCustomMenu.CATEGORY_LATE)
 	
-	self:Debug("<<1>> Loaded", self.displayName)
+	self:Info(GetString(TAKE_ONE_LOADED))
 
 end
 
 function TakeOne:DoTake(inventorySlot, _itemId)
-    self:Debug("    Entered DoTake")
-	
+    	
 	local slotType = ZO_InventorySlot_GetType(inventorySlot)
 	local bagId, slotIndex = ZO_Inventory_GetBagAndIndex(inventorySlot)
 	if not slotIndex then
-	    self:Debug("    Slot no longer available")
+	    self:Warn(GetString(TAKE_ONE_SLOT_FULL))
 	    PlaySound("Justice_PickpocketFailed")
 	    return
 	end
@@ -71,56 +142,57 @@ function TakeOne:DoTake(inventorySlot, _itemId)
 	local itemLink = GetItemLink(bagId, slotIndex)
 	local itemId = GetItemLinkItemId(itemLink)
 	if not( _itemId == itemId ) then
-	    self:Debug("   Slot contents changed")
+	    self:Warn(GetString(TAKE_ONE_SLOT_CHANGED))
 	    PlaySound("Justice_PickpocketFailed")
 	    return
 	end
 	
 	local targetSlot = FindFirstEmptySlotInBag(BAG_BACKPACK)
 	if not targetSlot then
-	    self:Debug("   No available slots in backpack")
+	    self:Warn(GetString(TAKE_ONE_NO_SLOTS))
 	    PlaySound("Justice_PickpocketFailed")
 		return
 	end
 	
 	local quantity = GetSlotStackSize(bagId, slotIndex)
-	self:Debug("    Moving 1 of <<1>>", quantity)
+	self:Debug(GetString(TAKE_ONE_DO_TAKE_ACTION), quantity)
 	
 	if slotType == SLOT_TYPE_BANK_ITEM then
   	    CallSecureProtected("RequestMoveItem", bagId, slotIndex, BAG_BACKPACK, targetSlot, 1)
 	elseif slotType == SLOT_TYPE_GUILD_BANK_ITEM then
 	    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, self:DoSplit(itemId, quantity))
 		EVENT_MANAGER:AddFilterForEvent(self.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_BAG_ID, BAG_BACKPACK)
-	    self:Debug("    Sending DoSplit for <<1>>, <<2>>", itemId, quantity)
+		
+	    self:Debug(GetString(TAKE_ONE_DO_TAKE_SENDING), itemId, quantity)
 		TransferFromGuildBank(slotIndex)	
 	else
 	    return
 	end
 	
-	self:Debug("    Leaving DoTake")
 end
 
 function TakeOne:DoSplit(itemId, quantity)
   return function(eventCode, bagId, slotIndex, isNewItem, itemSoundCategory, updateReason, stackCountChange)
-      self:Debug("    Entered DoSplit for <<1>>, <<2>>", itemId, quantity)
+
       if not( bagId == BAG_BACKPACK ) then
-          self:Debug("    Not in backpack: <<1>>", bagId)
+          self:Error(GetString(TAKE_ONE_DO_SPLIT_WRONG_BAG), bagId)
           return
       end
       
 	  local itemLink = GetItemLink(bagId, slotIndex)
 	  local _itemId = GetItemLinkItemId(itemLink)
 	  if not( _itemId == itemId ) then
-	      self:Debug("   Not the item we're looking for: <<1>> (<<2>>)", _itemId, itemId )
+	      self:Debug(GetString(TAKE_ONE_DO_SPLIT_WRONG_ITEM), _itemId, itemId )
 	      return
 	  end
 	  
       local _quantity = GetSlotStackSize(bagId, slotIndex)
       if not( quantity == _quantity ) then
-	      self:Debug("   Not the quantity we're expecting: <<1>> (<<2>>)", _quantity, quantity )
+	      self:Debug(GetString(TAKE_ONE_DO_SPLIT_WRONG_QUANTITY), _quantity, quantity )
           return
       end
-               
+      
+	  self:Debug(GetString(TAKE_ONE_DO_SPLIT_RIGHT_STACK), itemId, quantity)
       EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE)
                
       local targetSlot = FindFirstEmptySlotInBag(BAG_BACKPACK)
@@ -128,32 +200,29 @@ function TakeOne:DoSplit(itemId, quantity)
       
       EVENT_MANAGER:RegisterForEvent(self.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, self:DoReturn(bagId, slotIndex, targetSlot))
       EVENT_MANAGER:AddFilterForEvent(self.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_BAG_ID, BAG_BACKPACK)
-      self:Debug("    Sending DoReturn for <<1>>, <<2>>", _bagId, _slotIndex)
+	  
+      self:Debug(GetString(TAKE_ONE_DO_SPLIT_SENDING), _bagId, _slotIndex)
       CallSecureProtected("RequestMoveItem", bagId, slotIndex, BAG_BACKPACK, targetSlot, 1)
-               
-      self:Debug("    Leaving DoSplit")
+
   end
 end
 
 function TakeOne:DoReturn(bagId, slotIndex, targetSlot)
   return function(eventCode, _bagId, _slotIndex, isNewItem, itemSoundCategory, updateReason, stackCountChange)
-    self:Debug("    Entered DoReturn for <<1>>, <<2>>", bagId, slotIndex)
+    
     if not( targetSlot == _slotIndex ) then
-	   self:Debug("    <<1>> is not the slot we're looking for: <<2>>", targetSlot, _slotIndex)
+	   self:Debug(GetString(TAKE_ONE_DO_RETURN_WRONG_SLOT), targetSlot, _slotIndex)
        return
     end
     EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE)
     TransferToGuildBank( bagId, slotIndex)
-	
-	self:Debug("    Leaving DoReturn")
+
     end
 end
 
 function TakeOne:isValid(inventorySlot)
     local slotType = ZO_InventorySlot_GetType(inventorySlot)
     local bagId, slotIndex = ZO_Inventory_GetBagAndIndex(inventorySlot)
-	
-	self:Debug(" SlotType: <<1>>", slotType)
 	
     -- Check that this is a BANK or GUILD_BANK slot
 	if not( slotType == SLOT_TYPE_BANK_ITEM or slotType == SLOT_TYPE_GUILD_BANK_ITEM ) then
@@ -163,8 +232,6 @@ function TakeOne:isValid(inventorySlot)
 	-- Check that guild has a 
 	if slotType == SLOT_TYPE_GUILD_BANK_ITEM then
 	  local guildId = GetSelectedGuildBankId()
-	  
-	  self:Debug("    GuildId: <<1>>", guildId)
 	  
 	  if not guildId then
 	      return false
@@ -176,8 +243,6 @@ function TakeOne:isValid(inventorySlot)
 	  end
 	end
 	
-	self:Debug("   Has bank privileges")
-	
 	-- Check that the BACKPACK has enough room to operate
 	if slotType == SLOT_TYPE_BANK_ITEM and not CheckInventorySpaceSilently(1) then 
 	    return false
@@ -185,14 +250,10 @@ function TakeOne:isValid(inventorySlot)
 	    return false
 	end
 	
-	self:Debug("   Has enough bag space")
-	
 	-- Check that the source stack contains more than 1 item
 	if not (GetSlotStackSize(bagId, slotIndex) > 1) then
 	    return false
 	end
-	
-	self:Debug("   Source stack is large enough")
 	
 	return true
 end
@@ -209,12 +270,11 @@ function TakeOne:ShowContextMenu(inventorySlot, slotActions)
     local itemLink = GetItemLink(bagId, slotIndex)
 	local itemId = GetItemLinkItemId(itemLink)
 	
-	self:Debug(" BagID: <<1>>;  slotIndex: <<2>>; itemId: <<3>>", bagId, slotIndex, itemId)
+	self:Debug(GetString(TAKE_ONE_CONTEXT_MENU_INFO), bagId, slotIndex, itemId)
 	
-	AddCustomMenuItem(GetString(TO_CONTEXT_MENU), function() self:DoTake(inventorySlot, itemId) end)
+	AddCustomMenuItem(GetString(TAKE_ONE_CONTEXT_MENU), function() self:DoTake(inventorySlot, itemId) end)
 
 	 
 end
 
-ZO_CreateStringId("TO_CONTEXT_MENU", "Take 1")
 EVENT_MANAGER:RegisterForEvent(TakeOne.name, EVENT_ADD_ON_LOADED, function(...) TakeOne:OnAddOnLoaded(...) end)
